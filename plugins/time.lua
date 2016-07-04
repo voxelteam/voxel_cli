fdf58f905628e0ccc6f81e2b39ffe96009366918
voxelteam <voxeltaem@gmail.com>
2016-07-04 18:13:05 +0430

    Update time.lua

--- plugins/time.lua
+++ plugins/time.lua
@@ -1,15 +1,44 @@
+local color = {
+"red",
+"black",
+"blue",
+"green",
+"yellow",
+"magenta",
+"Orange",
+"DarkOrange",
+"Purple",
+"Golden",
+}
+local font = {
+"mathrm",
+"mathbf",
+"mathit",
+"mathfrak",
+"mathrm",
+}
+local size = {
+"300",
+"500",
+"700",
+"800",
+}
 local function run(msg, matches)
-if matches[1] == 'time' then
-    local cmd = io.popen('curl http://api.magic-team.ir/time.php')
-      local result = cmd:read('*all')
-    cmd:close()
-    return result
+local url , res = http.request('http://api.gpmod.ir/time/')
+if res ~= 200 then return "No connection" end
+local jdat = json:decode(url)
+local c = color[math.random(#color)]
+local s = size[math.random(#size)]
+local f = font[math.random(#font)]
+local url = "http://latex.codecogs.com/png.latex?".."\\dpi{"..s.."}%20\\huge%20\\"..f.."{{\\color{"..c.."}"..jdat.ENtime.."}}"
+local file = download_to_file(url,'time.webp')
+send_document(get_receiver(msg) , file, ok_cb, false)
 end
-end
-
+ 
 return {
-patterns = {
-"^[/#!][Tt](ime)$",
-},
-run = run
+  patterns = {
+    "^[Tt]ime$",
+    "^[!/#][Tt]ime$",
+  }, 
+  run = run 
 }
